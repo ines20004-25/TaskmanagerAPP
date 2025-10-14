c04a2a0f46591fb1d428d5d191fefc842de1242e
ines20004-25 <ineswederni123@gmail.com>
2025-10-14 12:17:53 +0100

    init

---
+++ src/app/app.ts
@@ -0,0 +1,30 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Task } from "./task/task";
+
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Task],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+
+  protected readonly title = signal('todolist');
+ 
+
+tasks = [
+{ id: 1, title: 'Learn Angular', done: false },
+{ id: 2,title: 'Build ToDoList App', done: false },
+{ id: 3, title: 'Celebrate!', done: true }
+];
+addNewTask(){
+ 
+  this.tasks.push({ id:4,title: 'Nex Task',done:false})
+}
+deleteTask(ind:number){
+  this.tasks.splice(ind,1)
+}
+}
+ 
