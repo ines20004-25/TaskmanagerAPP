e979600afbd406f687988d834f0ad90e9014142b
ines20004-25 <ineswederni123@gmail.com>
2025-11-11 10:22:57 +0100

    init

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,26 +1,25 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Task } from "./task/task";
-
-
+import { CurrencyPipe, DatePipe, DecimalPipe, LowerCasePipe, PercentPipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Taskservice } from './taskservice';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Task],
+  imports: [RouterOutlet, Task , UpperCasePipe,LowerCasePipe,TitleCasePipe,DatePipe,CurrencyPipe,PercentPipe,DecimalPipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-
+  today = new Date();
+  price = 1234.56;
+  percentage = 0.259;
   protected readonly title = signal('todolist');
- 
-
-tasks = [
-{ id: 1, title: 'Learn Angular', done: false },
-{ id: 2,title: 'Build ToDoList App', done: false },
-{ id: 3, title: 'Celebrate!', done: true }
-];
+tasks: any[] = [];
+constructor(private taskService: Taskservice) {}
+ngOnInit() {
+this.tasks = this.taskService.getTasks();
+}
 addNewTask(){
- 
   this.tasks.push({ id:4,title: 'Nex Task',done:false})
 }
 deleteTask(ind:number){
