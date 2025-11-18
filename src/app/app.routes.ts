265b5f1480128c5daa8ea42ecf6a5dcffa1ea2e2
ines20004-25 <ineswederni123@gmail.com>
2025-11-18 12:31:43 +0100

    aa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { App} from './app';
+import { About } from './about/about';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+{ path: 'about', component: About }
+];
