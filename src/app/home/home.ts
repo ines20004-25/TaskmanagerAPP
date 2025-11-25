93a7a9a26009de3f815a8ad772dcbe2f8f173b2f
ines20004-25 <ineswederni123@gmail.com>
2025-11-25 10:35:17 +0100

    ii

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
-
+import { Task } from '../task/task';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { Taskservice } from '../taskservice';
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Task,ReactiveFormsModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
+ 
 })
 export class Home {
+taskForm: any;
+constructor(private Taskservice: Taskservice,private fb: FormBuilder) {
+    this.taskForm = this.fb.group({
+        title: [''],
+        description: ['']
+      });
+  }
+
+
+ onSubmit() {
+      console.log(this.taskForm.value);
+       this.Taskservice.addTask(this.taskForm.value.title)  
+     
+    }
 
 }
